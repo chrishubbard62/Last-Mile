fc52d0520f1e4cb357c20fad0e45bae057436532
chris <chris.hubbard.dev@gmail.com>
2024-10-02 10:28:16 -0700

    updated tables will try to merge to main

--- app/models/delivery.py
+++ app/models/delivery.py
@@ -7,13 +7,20 @@ class Delivery(db.Model):
     __table_args__ = {'schema': SCHEMA}
 
   id = db.Column(db.Integer, primary_key=True)
+
   owner_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
   courier_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id'), ondelete='SET NULL'), nullable=True)
-  city = db.Column(db.String(50), nullable=False)
-  state = db.Column(db.String(50), nullable=False)
-  country = db.Column(db.String(50), nullable=False)
+
+  pickup_city = db.Column(db.String(50), nullable=False)
+  pickup_state = db.Column(db.String(50), nullable=False)
+  pickup_zip = db.Column(db.String(15), nullable=False)
   pickup_address = db.Column(db.String(150), nullable=False)
+
+  drop_city = db.Column(db.String(50), nullable=False)
+  drop_state = db.Column(db.String(50), nullable=False)
+  drop_zip = db.Column(db.String(15), nullable=False)
   drop_address = db.Column(db.String(150), nullable=False)
+
   description = db.Column(db.String(500), nullable=False)
   special_instructions = db.Column(db.String(500), nullable=True)
 
