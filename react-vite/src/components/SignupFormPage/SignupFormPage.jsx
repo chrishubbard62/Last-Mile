350731a6ebab48233c26ec13d1090cabcf77421b
chris <chris.hubbard.dev@gmail.com>
2024-10-13 08:35:37 -0700

    fixed email validation

--- react-vite/src/components/SignupFormPage/SignupFormPage.jsx
+++ react-vite/src/components/SignupFormPage/SignupFormPage.jsx
@@ -50,7 +50,7 @@ function SignupFormPage() {
         <div className='signup-inner-container'>
           <h1 className="title-h1">WELCOME TO</h1>
           <img className='login-signup-logo' src={logo} alt="logo" />
-          {errors.server && <p>{errors.server}</p>}
+          {errors.server && <p className="errors">{errors.server}</p>}
           <form className="signup-form" onSubmit={handleSubmit}>
             <label>
               Email
@@ -61,7 +61,7 @@ function SignupFormPage() {
               onChange={(e) => setEmail(e.target.value)}
               required
             />
-            {errors.email && <p>{errors.email}</p>}
+            {errors.email && <p className="errors">{errors.email}</p>}
             <label>
               Username
             </label>
@@ -71,7 +71,7 @@ function SignupFormPage() {
               onChange={(e) => setUsername(e.target.value)}
               required
             />
-            {errors.username && <p>{errors.username}</p>}
+            {errors.username && <p className="errors">{errors.username}</p>}
             <label>
               Password
             </label>
@@ -81,7 +81,7 @@ function SignupFormPage() {
               onChange={(e) => setPassword(e.target.value)}
               required
             />
-            {errors.password && <p>{errors.password}</p>}
+            {errors.password && <p className="errors">{errors.password}</p>}
             <label>
               Confirm Password
             </label>
@@ -91,7 +91,7 @@ function SignupFormPage() {
               onChange={(e) => setConfirmPassword(e.target.value)}
               required
             />
-            {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+            {errors.confirmPassword && <p className="errors">{errors.confirmPassword}</p>}
             <button className='signup-submit'type="submit">Sign Up</button>
           </form>
           <button onClick={() => navigate('/login')}>Back to Log In</button>
